330c1a3bb881897ced299d114270de04d9952e86
kononovDE <kononovde1@gmail.com>
2025-02-13 00:22:57 +0300

    Revert "Lesson 9 Rework"
    This reverts commit 361c1ebbabe5b5fb6d5ffea5c6962b5e63272e29.

--- src/test/com/urise/webapp/storage/FileStorageTest.java
+++ src/test/com/urise/webapp/storage/FileStorageTest.java
@@ -1,6 +1,7 @@
 package com.urise.webapp.storage;
 
 import urise.webapp.storage.FileStorage;
+import urise.webapp.storage.ObjectStreamFileStorage;
 import urise.webapp.storage.serializer.ObjectSerializer;
 
 public class FileStorageTest extends AbstractStorageTest {
